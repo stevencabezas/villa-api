bba33abd1e03cef7e2cf08789e63a610519c4971
stevencabezas <cabezasstevenr@gmail.com>
2023-10-11 15:24:48 -0600

    DbContext - dependencies inyections

---
+++ Villa_API/Migrations/20231011205321_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la Villa...", new DateTime(2023, 10, 11, 14, 53, 21, 357, DateTimeKind.Local).AddTicks(7424), new DateTime(2023, 10, 11, 14, 53, 21, 357, DateTimeKind.Local).AddTicks(7414), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle de la Villa...", new DateTime(2023, 10, 11, 14, 53, 21, 357, DateTimeKind.Local).AddTicks(7427), new DateTime(2023, 10, 11, 14, 53, 21, 357, DateTimeKind.Local).AddTicks(7426), "", 40, "Premium Vista a la Piscina...", 4, 150.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
--- Villa_API/Program.cs
+++ Villa_API/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Villa_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection"));
+});
 
 var app = builder.Build();
 
